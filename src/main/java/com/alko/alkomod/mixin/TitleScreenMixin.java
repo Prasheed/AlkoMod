6a16c8ef92e71e5a8446cfe7a7596341623ade69
Prasheed <danilsemenovnew@gmail.com>
2025-02-04 20:05:58 +0300

    Added Mixins to Project

---
+++ src/main/java/com/alko/alkomod/mixin/TitleScreenMixin.java
@@ -0,0 +1,15 @@
+package com.alko.alkomod.mixin;
+
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void onInit(CallbackInfo info) {
+        System.out.println("✔ Mixin работает! TitleScreen инициализирован.");
+    }
+}
