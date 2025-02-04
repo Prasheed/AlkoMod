56a905cb64a7055589156c3110da6bcb7755b67e
Prasheed <danilsemenovnew@gmail.com>
2025-02-04 20:53:14 +0300

    Added simple mixin to MainMenu(TitleScreen.class)

---
+++ src/main/java/com/alko/alkomod/mixin/ScreenAccessorMixin.java
@@ -0,0 +1,17 @@
+package com.alko.alkomod.mixin;
+
+
+import net.minecraft.client.gui.components.Renderable;
+import net.minecraft.client.gui.screens.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(Screen.class)
+public interface ScreenAccessorMixin {
+
+    //public final List<Renderable> renderables
+    @Accessor("renderables")
+    List<Renderable> getRenderables();
+}
