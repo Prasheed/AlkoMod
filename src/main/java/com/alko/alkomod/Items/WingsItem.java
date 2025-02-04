0491e54bd5b59fa40b1b64d182f267e094210f95
Rokko <4arik1337@gmail.com>
2025-02-04 22:18:04 +0300

    add LivingEntityAccessorMixin.java

--- src/main/java/com/alko/alkomod/Items/WingsItem.java
+++ src/main/java/com/alko/alkomod/Items/WingsItem.java
@@ -1,5 +1,6 @@
 package com.alko.alkomod.Items;
 
+import com.alko.alkomod.mixin.LivingEntityAccessorMixin;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterials;
@@ -18,26 +19,10 @@ public class WingsItem extends ArmorItem {
 
     @Override
     public void onArmorTick(ItemStack stack, Level level, Player player) {
-        if (!level.isClientSide) {
-            boolean isWearing = player.getInventory().getArmor(EquipmentSlot.CHEST.getIndex()).getItem() == this;
-
-            if (isWearing) {
-                player.getAbilities().mayfly = true;
-                player.onUpdateAbilities();
-            } else {
-                if (!player.isCreative() && !player.isSpectator()) {
-                    player.getAbilities().mayfly = false;
-                    player.getAbilities().flying = false;
-                    player.onUpdateAbilities();
-                }
-            }
-        }
-
-        // Логика управления полетом должна быть только на клиенте!
-        if (level.isClientSide && player.getAbilities().flying) {
-            Vec3 motion = player.getDeltaMovement();
-
-
+        boolean isWearing = player.getInventory().getArmor(EquipmentSlot.CHEST.getIndex()).getItem() == this;
+        boolean isJumping = ((LivingEntityAccessorMixin) player).is_jumping();
+        if (isJumping){
+            System.out.println("прыгаю");
         }
     }
 }
