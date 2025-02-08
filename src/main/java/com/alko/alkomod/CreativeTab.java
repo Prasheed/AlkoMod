03383dac8228efe4bef8aa1e0ec51573d64557f5
Prasheed <danilsemenovnew@gmail.com>
2025-02-08 03:23:49 +0300

    quick completion of Creative Tab

--- src/main/java/com/alko/alkomod/CreativeTab.java
+++ src/main/java/com/alko/alkomod/CreativeTab.java
@@ -3,6 +3,7 @@ package com.alko.alkomod;
 import com.alko.alkomod.Items.ModItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.ItemLike;
@@ -15,6 +16,8 @@ import java.util.function.Supplier;
 
 public class CreativeTab {
 
+    public static final ResourceLocation BACKGROUND = new ResourceLocation(Alkomod.MOD_ID,"textures/creative_tab_background.png");
+
     public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Alkomod.MOD_ID);
 
     public static final List<Supplier<? extends ItemLike>> ALKOMOD_TAB_ITEMS = new ArrayList<>();
@@ -26,6 +29,7 @@ public class CreativeTab {
                     .displayItems((displayParams, output) ->
                             ALKOMOD_TAB_ITEMS.forEach(itemLike -> output.accept(itemLike.get())))
                     .withSearchBar()
+                    .withBackgroundLocation(BACKGROUND)
                     .build()
     );
 
