d914771d68b222e12932a2791a1a2874202a5ca3
Rokko <4arik1337@gmail.com>
2025-02-17 21:17:42 +0300

    added some stuff

---
+++ src/main/java/com/alko/alkomod/Items/YandexBagItem.java
@@ -0,0 +1,11 @@
+package com.alko.alkomod.Items;
+
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+
+public class YandexBagItem extends ArmorItem {
+
+    public YandexBagItem(ArmorMaterial pMaterial, Type pType, Properties pProperties) {
+        super(pMaterial, pType, pProperties);
+    }
+}
